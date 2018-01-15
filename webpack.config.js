40a968d25d763cfd1e14836318ed30b0a61ea8ae
John serrano <andrey_201015@hotmail.com>
2018-01-15 11:39:32 -0500

    Agregando multiples entry points

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ const path = require('path');
 /* css-loaders, style-loader y  extract-text-webpack-plugin */
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+/*module.exports = {
   entry: path.resolve(__dirname, 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -45,7 +45,35 @@ module.exports = {
   plugins: [
     new ExtractTextPlugin('css/[name].css') //name permite uusar el nombre original del entrypoint
   ]
-}
+}*/
 
 /* Con este modulo podemos generar un file para los styles extract-text-webpack-plugin y se agregar al config como un plugin */
 // https://www.npmjs.com/package/extract-text-webpack-plugin
+
+
+/* Múltiples entry points */
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, 'src/js/index.js'),
+    precios: path.resolve(__dirname, 'src/js/precios.js'),
+    contacto: path.resolve(__dirname, 'src/js/contacto.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        }),
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/[name].css') //name permite uusar el nombre original del entrypoint
+  ]
+}
